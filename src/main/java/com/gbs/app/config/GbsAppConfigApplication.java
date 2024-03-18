d59d978318659dd03a5dc3f689476572c25d9147
jay-inexture <jayjiyani@inexture.com>
2024-03-18 10:48:59 +0530

    dev : init

---
+++ src/main/java/com/gbs/app/config/GbsAppConfigApplication.java
@@ -0,0 +1,13 @@
+package com.gbs.app.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbsAppConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbsAppConfigApplication.class, args);
+    }
+
+}
